66ef725ac08c277a3cf4104bd3032f64f0b42a09
m010 <sbashir@student.42.fr>
2024-11-24 17:08:40 +0300

    other

--- lua/moh/remap.lua
+++ lua/moh/remap.lua
@@ -1,3 +1,48 @@
+
+vim.opt.langmap = "йЙцЦуУкКеЕнНгГшШщЩзЗхХъЪфФыЫвВаАпПрРоОлЛдДжЖэЭяЯчЧсСмМиИтТьЬбБюЮ.\\,;qQwWeErRtTyYuUiIoOpP[{]}aAsSdDfFgGhHjJkKlL;:'\"zZxXcCvVbBnNmM\\,<.>/?"
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- TODO: use copilot
+-- toggle showing wornings 
+local show_warnings = false
+
+function ToggleWarnShow()
+  show_warnings = not show_warnings
+
+  if show_warnings then
+    vim.diagnostic.config({
+            virtual_text = {
+                severity = {min = vim.diagnostic.severity.HINT}
+            },
+--            signs = {
+--                severity = vim.diagnostic.severity.INFO,
+--            },
+--            underline = {
+--                severity = vim.diagnostic.severity.INFO,
+--            },
+        })
+        print("Show Warnings")
+  else
+    vim.diagnostic.config({
+            virtual_text = {
+                severity = vim.diagnostic.severity.ERROR,
+            },
+        })
+        print("Show only errors")
+  end
+end
+
+-- Перемещение между ошибками в lsp 
+vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>[", vim.diagnostic.goto_prev)
+
+vim.keymap.set("n", "<leader>tw", ToggleWarnShow)
+
+-- TODO remove if unused
+function Try()
+  vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR })
+end
+
+vim.keymap.set("n", "<leader>tr", Try)
